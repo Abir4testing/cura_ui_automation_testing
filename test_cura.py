c288a8a3b55859c4e0180017d8a218e4e2f4c969
Abir <158510880+Abir4testing@users.noreply.github.com>
2024-05-13 23:05:36 +0600

    Update test_cura.py

--- test_cura.py
+++ test_cura.py
@@ -1,5 +1,4 @@
 import openpyxl
-import pytest
 from selenium.webdriver.support.select import Select
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -10,10 +9,14 @@ import time
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 driver.get("https://katalon-demo-cura.herokuapp.com")
-driver.maximize_window()
+maximize_window = driver.maximize_window()
+maximize_window
 time.sleep(3)
+def case1(maximize_window):
+    return bool(maximize_window)
 
-
+def test_case1():
+    assert case1(driver.get_window_size()) == True
 
 path = "cura.xlsx"
 wb_obj = openpyxl.load_workbook(path)
@@ -24,20 +27,28 @@ password = sheet_obj.cell(row=1, column=2).value
 my_comment = sheet_obj.cell(row=1, column=3).value
 print(user_name, password, my_comment)
 
-
-
-
 make_appoinment = driver.find_element(By.XPATH, "/html[1]/body[1]/header[1]/div[1]/a[1]")
 make_appoinment.click()
 time.sleep(2)
 
-input_usernaem =  driver.find_element(By.XPATH, "/html[1]/body[1]/section[1]/div[1]/div[1]/div[2]/form[1]/div[2]/div[1]/input[1]")
+input_usernaem = driver.find_element(By.XPATH,
+                                     "/html[1]/body[1]/section[1]/div[1]/div[1]/div[2]/form[1]/div[2]/div[1]/input[1]")
 input_usernaem.send_keys(user_name)
-input_password = driver.find_element(By.XPATH, "/html[1]/body[1]/section[1]/div[1]/div[1]/div[2]/form[1]/div[3]/div[1]/input[1]")
+input_password = driver.find_element(By.XPATH,
+                                     "/html[1]/body[1]/section[1]/div[1]/div[1]/div[2]/form[1]/div[3]/div[1]/input[1]")
 input_password.send_keys(password + Keys.ENTER)
 time.sleep(1)
 
-select_facility = driver.find_element(By.XPATH, "/html[1]/body[1]/section[1]/div[1]/div[1]/form[1]/div[1]/div[1]/select[1]")
+make_appoinment = driver.find_element(By.XPATH, "/html[1]/body[1]/section[1]/div[1]/div[1]/div[1]/h2[1]")
+
+def case2(make_appoinment):
+    return bool(make_appoinment)
+
+def test_case2():
+    assert case2(make_appoinment) == True
+
+select_facility = driver.find_element(By.XPATH,
+                                      "/html[1]/body[1]/section[1]/div[1]/div[1]/form[1]/div[1]/div[1]/select[1]")
 select_facility.click()
 time.sleep(1)
 
@@ -45,36 +56,44 @@ my_select_facility = Select(select_facility);
 my_select_facility.select_by_visible_text("Seoul CURA Healthcare Center")
 time.sleep(1)
 
-apply_hospital_readmission = driver.find_element(By.XPATH, "/html[1]/body[1]/section[1]/div[1]/div[1]/form[1]/div[2]/div[1]/label[1]/input[1]")
+apply_hospital_readmission = driver.find_element(By.XPATH,
+                                                 "/html[1]/body[1]/section[1]/div[1]/div[1]/form[1]/div[2]/div[1]/label[1]/input[1]")
 apply_hospital_readmission.click()
 time.sleep(1)
 
-select_program = driver.find_element(By.XPATH, "/html[1]/body[1]/section[1]/div[1]/div[1]/form[1]/div[3]/div[1]/label[2]/input[1]")
+select_program = driver.find_element(By.XPATH,
+                                     "/html[1]/body[1]/section[1]/div[1]/div[1]/form[1]/div[3]/div[1]/label[2]/input[1]")
 select_program.click()
 time.sleep(1)
 
-select_calendar = driver.find_element(By.XPATH, "/html[1]/body[1]/section[1]/div[1]/div[1]/form[1]/div[4]/div[1]/div[1]/div[1]/span[1]")
+select_calendar = driver.find_element(By.XPATH,
+                                      "/html[1]/body[1]/section[1]/div[1]/div[1]/form[1]/div[4]/div[1]/div[1]/div[1]/span[1]")
 select_calendar.click()
 time.sleep(1)
 
-input_select_calendar = driver.find_element(By.XPATH, "/html[1]/body[1]/section[1]/div[1]/div[1]/form[1]/div[4]/div[1]/div[1]/input[1]")
+input_select_calendar = driver.find_element(By.XPATH,
+                                            "/html[1]/body[1]/section[1]/div[1]/div[1]/form[1]/div[4]/div[1]/div[1]/input[1]")
 input_select_calendar.click()
 input_select_calendar.send_keys("06/10/2002")
 time.sleep(1)
 
-input_comment = driver.find_element(By.XPATH, "/html[1]/body[1]/section[1]/div[1]/div[1]/form[1]/div[5]/div[1]/textarea[1]")
+input_comment = driver.find_element(By.XPATH,
+                                    "/html[1]/body[1]/section[1]/div[1]/div[1]/form[1]/div[5]/div[1]/textarea[1]")
 input_comment.click()
 input_comment.send_keys(my_comment)
 time.sleep(1)
 
-input_book_appointment = driver.find_element(By.XPATH, "/html[1]/body[1]/section[1]/div[1]/div[1]/form[1]/div[6]/div[1]/button[1]")
+input_book_appointment = driver.find_element(By.XPATH,
+                                             "/html[1]/body[1]/section[1]/div[1]/div[1]/form[1]/div[6]/div[1]/button[1]")
 input_book_appointment.click()
 time.sleep(2)
 
 actual_title = driver.find_element(By.XPATH, "//*[text()='Appointment Confirmation']")
 
-def case1(actual_title):
+
+def case3(actual_title):
     return bool(actual_title)
 
-def test_case1():
-    assert case1("Appointment Confirmation") == True
+
+def test_case3():
+    assert case1(actual_title) == True
